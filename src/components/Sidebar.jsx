71d0b75e932531e31f484b3d494fefdf98d5769d
HungHyperX <131465286+HungHyperX@users.noreply.github.com>
2024-10-10 21:11:32 +0700

    tao Hung-branch va vai thu co ban

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,24 @@ import styled from "styled-components";
 export default function Sidebar() {
   return (
     <Container>
-      SIDEBAR
+      <div className="top__links">
+        <div className="logo">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCpYrVsWwufHstOUb2Xmi4Q-EjwAB0jmhl8y071LKMTmZFR5WlSkCXpHafKHqGdTv3YGM&usqp=CAU"
+            alt="Spotify-Clone-Logo"
+          />
+        </div>
+        <ul>
+          <li>
+            <span>Home</span>
+          </li>
+          <li>
+            <span>Search</span>
+          </li>
+          <li>
+            <span>Your Library</span>
+          </li>
+        </ul>
+      </div>
     </Container>
   );
 }
