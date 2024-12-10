88cb8ec656ea289ec6ac62051704630ec929b5b8
havleoo <hung28092003@gmail.com>
2024-12-10 19:29:03 +0700

    Revert "Merge pull request #38 from havleoo/0405"
    This reverts commit dad32aab39171ed338e8d16e33de2ed5f24701b8, reversing
    changes made to fc2dc9dfd91b9d0fecf20484f1560ec9e0723f37.

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -3,8 +3,6 @@ import styled from "styled-components";
 import Playlists from "./Playlists";
 import { useStateProvider } from "../utils/StateProvider";
 import { reducerCases } from "../utils/Constants";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function Sidebar() {
   const [showCreateInput, setShowCreateInput] = useState(false); // Trạng thái để hiển thị input
@@ -32,10 +30,13 @@ export default function Sidebar() {
           <li onClick={handleHomeClick}>
             <span>Home</span>
           </li>
+          <li>
+            <span>Search</span>
+          </li>
           <li className="library">
             <span>Your Library</span>
             <button onClick={() => setShowCreateInput(!showCreateInput)}>
-              <FontAwesomeIcon icon={faPlus}/>
+              +
             </button>
           </li>
         </ul>
